cd9e0876a058b5ca7e5c0d8d60078932b33b0a49
Anoncer <anoncer@protonmail.com>
2022-09-04 13:48:18 +0200

    Add Desctop Application

---
+++ ZipStock.Desctop/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZipStock.Desctop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
