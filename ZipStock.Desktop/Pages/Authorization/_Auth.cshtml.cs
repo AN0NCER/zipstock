f3254b6a7bcfbdd375487207c054ee2bf357d567
Anoncer <anoncer@protonmail.com>
2022-09-30 13:01:03 +0200

    Page auth

---
+++ ZipStock.Desktop/Pages/Authorization/_Auth.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.JSInterop;
+
+namespace ZipStock.Desktop.Pages.Authorization
+{
+    public class _AuthModel : PageModel
+    {
+        public _AuthModel()
+        {
+            Electron.IpcMain.On("async-msg", (data) =>
+            {
+                Electron.Dialog.ShowMessageBoxAsync(data.ToString());
+            });
+
+            Electron.IpcMain.On("verification-email", (data)=>{
+                string email = (string)data;
+            });
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ZipStock.Desktop/wwwroot/js/authorization.js
@@ -0,0 +1,19 @@
+﻿const { ipcRenderer } = require("electron");
+
+
+
+function BtnClicked() {
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+    if (validateEmail($('.email').val())) {
+        ipcRenderer.send("verification-email", $('.email').val()));
+    } else {
+        ipcRenderer.send("async-msg", "Bad Email");
+    }
+};
+
