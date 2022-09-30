a7e60151755906027a2fe82dba5f3046ef796980
Anoncer <anoncer@protonmail.com>
2022-09-30 13:08:34 +0200

    Add to project Desctop project Server

---
+++ ZipStock.Server/Verification.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace ZipStock.Server
+{
+    public class Verification
+    {
+        private string _email { get; set; }
+
+        public Verification(string email)
+        {
+            _email = email;
+        }
+
+        public void VerificateEmail()
+        {
+            string response = HttpGet($"http://zipstock.click/api/code-verification.php?email={_email}");
+        }
+
+        public string HttpGet(string uri)
+        {
+            string content = null;
+
+            var wc = new WebClient();
+            content = wc.DownloadString(uri);
+
+            return content;
+        }
+    }
+}
+
